2ea45990c940f7716e517a0ec5ede83a4b402ec3
tonyhieu <54915685+tonyhieu@users.noreply.github.com>
2021-08-23 23:26:45 -0700

    fix app.py to let make it easier to run

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template
 
-
 app = Flask("app")
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/anthony")
 def anthony():
     return render_template("individual/anthony.html")
+
 
 @app.route("/ellen")
 def ellen():
     return render_template("individual/ellen.html")
 
-app.run(host="127.0.0.1", port=8080)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080)
