2a13ef02bbbf8973851817fa4b87567a260cbc6c
tonyhieu <amv7668@gmail.com>
2021-10-20 06:48:41 -0700

    improve api (likes dislikes, specific id)

--- api/imageapi.py
+++ api/imageapi.py
@@ -6,16 +6,37 @@ api_bp = Blueprint('api', __name__,
                    static_folder='static', static_url_path='static/api')
 
 images = []
+image_links = [
+    "https://image.shutterstock.com/image-photo/wide-panorama-sunset-sky-clouds-260nw-731576737.jpg",
+    "https://i.redd.it/j79d4w01vmn01.jpg",
+    "https://www.710beachrentals.com/wp-content/uploads/2017/02/La-Jolla-cove.jpg",
+    "https://a0.muscache.com/pictures/f870caa5-509e-4802-aa30-85dc27f358e0.jpg",
+    "https://theshoallajolla.com/latest/wp-content/uploads/2019/02/shutterstock_217914379.jpg"
+]
+image_titles = [
+    "Sunset",
+    "Beach1",
+    "La Jolla Cove",
+    "Beach2",
+    "Beach3"
+]
+
 
 def _init_images ():
-    images.append({"id": 1, "title": "Sunset", "url": "https://image.shutterstock.com/image-photo/wide-panorama-sunset-sky-clouds-260nw-731576737.jpg"})
-    images.append({"id": 2, "title": "Beach1", "url": "https://i.redd.it/j79d4w01vmn01.jpg"})
-    images.append({"id": 3, "title": "La Jolla Cove", "url": "https://www.710beachrentals.com/wp-content/uploads/2017/02/La-Jolla-cove.jpg"})
-    images.append({"id": 4, "title": "Beach2", "url": "https://a0.muscache.com/pictures/f870caa5-509e-4802-aa30-85dc27f358e0.jpg"})
-    images.append({"id": 5, "title": "Beach3", "url": "https://theshoallajolla.com/latest/wp-content/uploads/2019/02/shutterstock_217914379.jpg"})
+    for i in range(len(image_links)):
+        images.append({"id": i+1, "title": image_titles[i], "link": image_links[i], "likes": 0, "dislikes": 0})
 
 @api_bp.route('/images')
 def get_images():
     if len(images) == 0:
         _init_images()
     return jsonify(images)
+
+@api_bp.route('/images/<id>')
+def get_image(id):
+    if len(images) == 0:
+        _init_images()
+    try:
+        return jsonify(images[int(id)-1])
+    except:
+        return "Invalid ID"
