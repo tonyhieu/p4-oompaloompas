e0cdcdf3251e103a18c0ba7bcb73bf730f9f30ec
LindaLiu1202 <lin.ltl.135135@gmail.com>
2021-11-02 00:19:30 -0700

    Individual Binary Lab

--- app.py
+++ app.py
@@ -184,6 +184,24 @@ def onclick():
 def lindalogic():
     return render_template("individual/Linda/logic.html")
 
+
+@app.route("/lindaBinary", methods=['GET','POST'])
+def lindaBinary():
+    if request.form:
+        try:
+            bits = request.form.get("bits")
+
+            if request.form["picture_change"] == "Light Bulbs": # checks which button is pressed; if light bulbs button is pressed, light bulbs are the picture and vice versa
+                return render_template("individual/Linda/lindaBinary.html", bits=int(bits), image_path_on="/static/assets/bulb_on.gif", image_path_off="/static/assets/bulb_off.png")
+            if request.form["picture_change"] == "Switch":
+                return render_template("individual/Linda/lindaBinary.html", bits=int(bits), image_path_on="/static/assets/pictures/on.png", image_path_off="/static/assets/pictures/off.png")
+
+            # starting and empty input default
+        except:
+            return render_template("individual/Linda/lindaBinary.html", bits=8, image_path_on="/static/assets/pictures/on.png", image_path_off="/static/assets/pictures/off.png")
+    return render_template("individual/Linda/lindaBinary.html", bits=8, image_path_on="/static/assets/bulb_on.gif", image_path_off="/static/assets/bulb_off.png")
+
+
 @app.route('/sanvi', methods=['GET', 'POST'])
 def sanvi():
     if request.form:
